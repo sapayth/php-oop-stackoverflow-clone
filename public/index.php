311ab33fcb870124d0983460a12cf9c4d45f2d3c
Atik Hassan <atikhassan.cse@gmail.com>
2020-04-11 14:29:11 +0600

    db connection done

--- public/index.php
+++ public/index.php
@@ -1,8 +1,12 @@
 <?php
 
 include '../src/Database/ShohanConnection.php';
+include '../src/Database/AtikConnection.php';
 
-$databaseConnectionInstance = new ShohanConnection('localhost','stack_faiyaz', 'root', '');
+//$databaseConnectionInstance = new ShohanConnection('localhost','stack_faiyaz', 'root', '');
+
+//$databaseConnectionInstanceFromAtik = new AtikConnection('localhost', 'root', '', 'test');
+#$db = $databaseConnectionInstanceFromAtik->getDB();
 
 // Configure connection parameters.
 
