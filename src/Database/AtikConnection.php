c2943861f4dcbbd2ac05000415700b6986504fb4
Atik Hassan <atikhassan.cse@gmail.com>
2020-04-11 21:02:06 +0600

    removed interface and refactor code

--- src/Database/AtikConnection.php
+++ src/Database/AtikConnection.php
@@ -6,10 +6,10 @@
  * Time: 11:08 AM
  */
 
-class AtikConnection implements ConnectionInterface
+class AtikConnection
 {
     protected $db;
-    private $hostname, $username, $password, $database, $charset;
+    private $hostname, $username, $password, $database;
 
 
     /**
@@ -18,22 +18,20 @@ class AtikConnection implements ConnectionInterface
      * @param $username
      * @param $password
      * @param $database
-     * @param string $charset
      */
-    public function __construct($hostname, $username, $password, $database, $charset = "utf8mb4")
+    public function __construct($hostname, $username, $password, $database)
     {
         $this->hostname = $hostname;
         $this->username = $username;
         $this->password = $password;
         $this->database = $database;
-        $this->charset = $charset;
         $this->connect();
 
     }
 
     public function __sleep()
     {
-        return [$this->hostname, $this->username, $this->password, $this->database, $this->charset];
+        return [$this->hostname, $this->username, $this->password, $this->database];
     }
 
     public function __wakeup()
@@ -44,12 +42,10 @@ class AtikConnection implements ConnectionInterface
     private function connect()
     {
         $dsn = "mysql:host={$this->hostname};
-                dbname={$this->database};
-                charset={$this->charset}";
+                dbname={$this->database};";
         $options = [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, # convert error to exception
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC   # set default fetch mode associative array
-            #PDO::ATTR_EMULATE_PREPARES      => false
         ];
 
         try {
@@ -61,7 +57,7 @@ class AtikConnection implements ConnectionInterface
 
     }
 
-    public function getDB(){
+    public function getConnection(){
         if(!isset($this->db))
             $this->connect();
         return $this->db;
