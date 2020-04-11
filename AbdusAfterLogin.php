f82cfba6b2b40f46db436f0ce2038fbd5b8f2cf1
Abdus Sukkur <abdussukkur73@gmail.com>
2020-04-11 15:22:01 +0600

    Session start, Created setAuthenticate function for store user data in _SESSION varaible and create logout function

--- AbdusAfterLogin.php
+++ AbdusAfterLogin.php
@@ -1,4 +1,20 @@
 <?php
 class AbdusAfterLogin{
-    
+    public function __construct()
+    {
+        session_start();
+        $_SESSION['loggedin'] = false;
+        $_SESSION['username'] = false;
+    }
+
+    public function setAuthenticate($usename){
+        $_SESSION['loggedin'] = true;
+        $_SESSION['username'] = $usename;
+    }
+
+    public function logout(){
+        unset($_SESSION['loggedin']);
+        unset($_SESSION['username']);
+        session_destroy();
+    }
 }
