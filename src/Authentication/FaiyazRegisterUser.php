43a345986133b623c2a887bddac384f1b32b95f5
Faiyaz Rahman <faiyazrahman03@gmail.com>
2020-04-11 08:15:07 +0100

    Created database connection

--- src/Authentication/FaiyazRegisterUser.php
+++ src/Authentication/FaiyazRegisterUser.php
@@ -2,5 +2,12 @@
 
 class FaiyazRegisterUser
 {
+    private $db_username = "root";
+    private $db_password = "";
+    public $connection;
 
+    public function __construct()
+    {    
+        $this->connection = new PDO('mysql:host=localhost;dbname=stack_overflow_clone', $this->db_username, $this->db_password);
+    }
 }
