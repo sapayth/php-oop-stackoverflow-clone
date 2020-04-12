eecb510e0f13f077dbf7a3f6e2a1819d93fcce34
Abdus Sukkur <abdussukkur73@gmail.com>
2020-04-13 01:36:09 +0600

    write code for autloading file

---
+++ autoload.php
@@ -0,0 +1,6 @@
+<?php
+spl_autoload_register(function ($className) {
+    $path = strtolower(str_replace("StackOverflowClone\\", "", $className).".php");
+    $path = str_replace("\\", "/", $path);
+    include_once($path);
+});
