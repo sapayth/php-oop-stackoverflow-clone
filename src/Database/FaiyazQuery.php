2a08fd0548dd03ab37a436f3c4039efd2632b06d
Faiyaz Rahman <faiyazrahman03@gmail.com>
2020-04-13 23:18:18 +0600

    Created the getUser method to read users

--- src/Database/FaiyazQuery.php
+++ src/Database/FaiyazQuery.php
@@ -8,6 +8,21 @@ include_once '../../autoload.php';
 
 class FaiyazQuery extends FaiyazConnection
 {
-    
+    //Using these method you can get all the users data just using username
+    public function getUser($username)
+    {
+        $sql = "SELECT * FROM `users` WHERE username = ?";
+        $stmt = $this->connect()->prepare($sql);
+        $stmt->execute([$username]);
+        $users = $stmt->fetchAll();
+
+        foreach($users as $user){
+            echo "Name is ". $user['username'] . "<br>";
+            echo "Password is " . $user['password'] . "<br>";
+        }
+    }
 }
+
+// $user = new FaiyazQuery();
+// $user->getUser('faiyaz');
 
