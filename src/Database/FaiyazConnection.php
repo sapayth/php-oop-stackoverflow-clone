c919370c1d46490c0d6c0fd48098bc18d6c3d47f
Faiyaz Rahman <faiyazrahman03@gmail.com>
2020-04-14 18:47:32 +0600

    Created update method to update data on Database

--- src/Database/FaiyazConnection.php
+++ src/Database/FaiyazConnection.php
@@ -11,6 +11,11 @@ class FaiyazConnection
    public function connect()
    {
       $pdo = new PDO('mysql:host=localhost;dbname=stack_overflow_clone', $this->db_username, $this->db_password);
+
+      //turn on exceptions
+      $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+      //set default fetch mode
       $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
       return $pdo;
    }
--- src/Database/FaiyazQuery.php
+++ src/Database/FaiyazQuery.php
@@ -9,13 +9,16 @@ include_once '../../autoload.php';
 
 class FaiyazQuery extends FaiyazConnection
 {
-    //Using these method you have to just pass the table name and value as a array. Thenv value wibb be inserted on database 
+    //Using these method you have to just pass the table name and value as a array. Thenv value wibb be inserted on database
     public function insert($table, array $data)
     {
         try {
             if (is_array($data)) {
+                //Build the Dynamic Query
                 $columnNames = join(',', array_keys($data));
                 $columnValues = ':' . join(', :', array_keys($data));
+
+                //Query and Execute
                 $sql = "INSERT INTO $table ($columnNames) VALUES ($columnValues)";
                 $stmt = $this->connect()->prepare($sql);
                 $stmt->execute($data);
@@ -24,11 +27,48 @@ class FaiyazQuery extends FaiyazConnection
             $e->getMessage();
         }
     }
+
+    ////To use these method you have to pass these 3 param and you can easily update your database info
+    public function update($table, $data, $where)
+    {
+        try {
+            //set columns
+            $columns = null;
+            foreach ($data as $key => $value) {
+                $columns .= "$key = :$key,";
+            }
+
+            //Trim the right side of the column for avoid silly mistakes
+            $columns = rtrim($columns, ',');
+
+            //se Where
+            $whereColumns = null;
+            foreach ($where as $key => $value) {
+                $whereColumns .= "$key = :$key,";
+            }
+
+            $whereColumns = rtrim($whereColumns, ',');
+
+            //prepare Statement
+            $sql = "UPDATE $table SET $columns WHERE $whereColumns";
+            $stmt = $this->connect()->prepare($sql);
+
+            //bind Values through foreeach loop
+            foreach ($data as $key => $value) {
+                $stmt->bindValue(":$key", $value);
+            }
+            foreach ($where as $key => $value) {
+                $stmt->bindValue(":$key", $value);
+            }
+
+            $stmt->execute();
+            return $stmt->rowCount();
+
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        }
+    }
 }
 
 // $user = new FaiyazQuery();
-// $data = [
-//     "username" => 'faiyaz',
-//     "password" =>  password_hash('Pass1436', PASSWORD_DEFAULT),
-// ];
-// $user->insert("users", $data);
+// $user->update('users', $data = ['username' => 'Faiyaz15', 'password' => 'Passsword15'], $where = ['id' => 15]);
