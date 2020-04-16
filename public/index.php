28ff09df9c5be06507a68657289c6e48adea5b1a
Abdus Sukkur <abdussukkur73@gmail.com>
2020-04-16 20:58:23 +0600

    created Authentication.php file and merge all code in the file from all individual file

--- public/index.php
+++ public/index.php
@@ -1,6 +1,8 @@
 <?php
 
-require_once __DIR__ . "/../autoload.php";
+use Src\Database\AtikConnection;
+
+require_once "../autoload.php";
 // include '../src/Database/ShohanConnection.php';
 // include '../src/Database/AtikConnection.php';
 
@@ -10,7 +12,7 @@ require_once __DIR__ . "/../autoload.php";
 // include '../src/Database/AtikConnection.php';
 //echo $databaseConnectionInstance = new ShohanConnection('localhost', 'rony', 'root', '');
 
-//$databaseConnectionInstanceFromAtik = new AtikConnection('localhost', 'root', '', 'rony');
+$databaseConnectionInstanceFromAtik = new AtikConnection('localhost', 'root', '', 'rony');
 //$db = $databaseConnectionInstanceFromAtik->getConnection();
 
 // Configure connection parameters.
