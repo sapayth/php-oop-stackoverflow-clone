38eeebc1643d5bcb05c661e2dadd99059c29a35f
Naowas Morshed Eimon <naowas.morshed.eimon@gmail.com>
2020-04-17 17:30:59 +0600

    some query has been created
    getAll, getById, droptable, deteteAll, deleteById

--- src/Database/NaowasQuery.php
+++ src/Database/NaowasQuery.php
@@ -27,13 +27,63 @@ class NaowasQuery
 
     }
 
-    public function BasicQuery($tableName)
+    public function getAll($tableName)
     {
 
-        $query = $this->connect_db->query("select * comments");
-        $query->bindParam(":tableName", $tableName);
+        $query = $this->connect_db->query("select * $tableName");
+        $query->execute();
+        $results = $query->fetchAll(PDO::FETCH_OBJ);
+        foreach ($results as $value) {
+            return $value;
+        }
+
+    }
+
+    public function getById($id, $tableName)
+    {
+        $query = $this->connect_db->query("SELECT * FROM $tableName WHERE id = $id");
+        $query->execute([$id]);
+        $results = $query->fetch(PDO::FETCH_OBJ);
+        foreach ($results as $value) {
+            return $value;
+        }
+    }
+
+    public function insert($tableName)
+    {
+        if (isset($_POST['submit'])) {
+            $query = $this->connect_db->query("");
+        }
+    }
+
+    public function droptable($tableName)
+    {
+        $query = $this->connect_db->query("RUNCATE TABLE $tableName");
         $query->execute();
 
+    }
+
+    public function deleteById($tableName, $id)
+    {
+        $query = $this->connect_db->query("DELETE FROM $tableName WHERE id = $id");
+        $query->execute([$id]);
+        if ($query = $this->connect_db->query($query)) {
+            echo "Success";
+        } else {
+            echo "Failed";
+        }
+
+    }
+
+    public function deleteAll($tableName)
+    {
+        $query = $this->connect_db->query("DELETE * FROM $tableName");
+        $query->execute();
+        if ($query = $this->connect_db->query($query)) {
+            echo "Success";
+        } else {
+            echo "Failed";
+        }
     }
 
 }
