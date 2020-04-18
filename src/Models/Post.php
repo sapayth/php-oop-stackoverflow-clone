87fd31d3b31cc7563ce0e91eb9ba74faf03d7831
Faiyaz Rahman <faiyazrahman03@gmail.com>
2020-04-18 21:48:47 +0600

    Created allPost method on Post.php

--- src/Models/Post.php
+++ src/Models/Post.php
@@ -9,5 +9,18 @@ include_once '../../autoload.php';
 
 class Post extends FaiyazQuery
 {
-    
+    public function allPost()
+    {
+        try {
+
+            $posts = $this->getAll('posts');
+            return $posts;
+
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        }
+    }
 }
+
+$post = new Post();
+//print_r($post->allPost());
