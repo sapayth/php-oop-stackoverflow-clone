94e40c4bf56ef1ed509b564359deecc4aca940f2
Faiyaz Rahman <faiyazrahman03@gmail.com>
2020-04-18 16:30:42 +0600

    Created getPost method

--- src/Models/User.php
+++ src/Models/User.php
@@ -2,7 +2,20 @@
 
 namespace Src\Models;
 
-class User
-{
+use PDOException;
+use Src\Database\FaiyazQuery;
 
+include_once '../../autoload.php';
+
+class User extends FaiyazQuery
+{
+    public function getPost()
+    {
+       try{
+          $posts = $this->getAll('posts');
+           return $posts;
+       }catch (PDOException $e){
+           echo $e->getMessage();
+       }
+    }
 }
