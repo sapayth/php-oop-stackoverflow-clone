56c40254e2c0f5ee553ef1e685e3e52bd3fb1361
Faiyaz Rahman <faiyazrahman03@gmail.com>
2020-04-25 14:05:15 +0600

    FaiyazRolePermission class created

---
+++ src/Role_Permission/FaiyazRolePermission.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Src\Role_Permission;
+
+use Src\Authentication\FaiyazRoleBasedAuth;
+
+include_once '../../autoload.php';
+
+class FaiyazRolePermission extends FaiyazRoleBasedAuth
+{
+    
+}
